fae50cb5f2b3d963a82b7b5853e5dadb65a153ec
James McManamey <jfmcmanamey@gmail.com>
2018-12-06 15:15:38 -0800

    12 tests passing

--- index.js
+++ index.js
@@ -21,23 +21,15 @@ var testdata = [
 var dataset = testdata
 
 
+
+
+
 //defaults for the chart
 const height = 400
 const width = 800
 const padding = 40
-var barWidth = (width - padding * 2) / dataset.length;
+
 const title = "Some GDP Numbers"
-
-//transformers to make sure the element fits inside the box 
-
-const xScale = d3.scaleBand()
-  .range([padding, width - padding])
-  .domain(dataset.map((d) => d[0]))
-
-const yScale = d3.scaleLinear()
-  .domain([0, d3.max(dataset, (d) => d[1])])
-  .range([height - padding, padding]);
-
 
 const chartContainer = d3.select('#chart')
 
@@ -51,10 +43,10 @@ const chart = chartContainer
   .attr('class', 'chart')
 
 const tooltip = d3.select('#chart')
-.append('div')
-.attr('class', 'tooltip')
-.attr('id', 'tooltip')
-  
+  .append('div')
+  .attr('class', 'tooltip')
+  .attr('id', 'tooltip')
+
 
 function getOffset(el) {
   const rect = el.getBoundingClientRect();
@@ -69,57 +61,82 @@ function getOffset(el) {
 //TODO call to fetch the data will go here 
 
 
-
-var bar = chart.selectAll('g')
-  .data(dataset)
-  .enter()
-  .append('g')
-  .append('rect')
-  .attr("class", "bar")
-  .attr("data-date", (d) => d[0])
-  .attr("data-gdp", (d) => [1])
-
-  .attr('width', barWidth - 1)
-  .attr('height', (d) => (height - padding) - yScale(d[1]))
-
-  .attr('y', (d) => yScale(d[1]))
-  .attr('x', (d, i) => padding + i * barWidth)
-  .on('mouseover', function(d,i){})
-
-  
-  //adding tooltips
-  .on("mouseover", function (d,i) {
-    tooltip.transition()
-    .style('left',getOffset(chartContainer._groups[0][0]).left + padding + (i+1) * barWidth  )
-    .style('top', getOffset(chartContainer._groups[0][0]).top + 0.75 *height  )
-   
-    .attr("data-date", d[0])
-    .attr("data-gdp", d[1])
-    .style('opacity', 0.9)
-    .text(d[1])
-    console.log(d)
-      
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+  .then(function (response) {
+    return response.json();
   })
-  .on("mouseout", function (el) {
-    tooltip.transition()
-      .style("opacity", 0);
-  });
+  .then(function (myJson) {
+    dataset = myJson.data
 
-//adding axis
-const xAxis = d3.axisBottom(xScale);
 
-chart.append("g")
-  .attr("id", "x-axis")
-  .attr("transform", "translate(0, " + (height - padding) + ")")
-  .call(xAxis);
 
-const yAxis = d3.axisLeft(yScale);
 
-chart.append("g")
-  .attr("id", "y-axis")
-  .attr("transform", "translate(" + padding + ",0)")
-  .call(yAxis);
-let ticks = d3.selectAll(".tick text");
-  ticks.attr("class", "tick")
+    //transformers to make sure the element fits inside the box 
 
+    const barWidth = (width - padding * 2) / dataset.length;
+
+    const xScale = d3.scaleTime()
+      .range([padding, width - padding])
+      .domain(d3.min(dataset, (d)=> d[0]),d3.max(dataset, (d)=> d[0]))
+     
+
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(dataset, (d) => d[1])])
+      .range([height - padding, padding]);
+
+
+    //adding bars 
+    var bar = chart.selectAll('g')
+      .data(dataset)
+      .enter()
+      .append('g')
+      .append('rect')
+      .attr("class", "bar")
+      .attr("data-date", (d) => d[0])
+      .attr("data-gdp", (d) => d[1])
+
+      .attr('width', barWidth)
+      .attr('height', (d) => (height - padding) - yScale(d[1]))
+
+      .attr('y', (d) => yScale(d[1]))
+      .attr('x', (d, i) => padding + i * barWidth)
+      .on('mouseover', function (d, i) { })
+
+
+      //adding tooltips
+      .on("mouseover", function (d, i) {
+        tooltip.transition()
+          .style('left', getOffset(chartContainer._groups[0][0]).left + padding * 1.5 + (i + 1) * barWidth)
+          .style('top', getOffset(chartContainer._groups[0][0]).top + 0.7 * height)
+
+          .attr("data-date", d[0])
+          .attr("data-gdp", d[1])
+          .style('opacity', 0.9)
+          .text(d[1])
+       
+
+      })
+      .on("mouseout", function (el) {
+        tooltip.transition()
+          .style("opacity", 0);
+      });
+
+    //adding the axis
+    const xAxis = d3.axisBottom(xScale)
+
+    chart.append("g")
+      .attr("id", "x-axis")
+      .attr("transform", "translate(0, " + (height - padding) + ")")
+      .call(xAxis);
+
+    const yAxis = d3.axisLeft(yScale);
+
+    chart.append("g")
+      .attr("id", "y-axis")
+      .attr("transform", "translate(" + padding + ",0)")
+      .call(yAxis);
+    let ticks = d3.selectAll(".tick text");
+    ticks.attr("class", "tick")
+
+  })
 
